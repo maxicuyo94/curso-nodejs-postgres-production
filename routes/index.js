dfbdf4c6b302b2ec06f89a7bb46f5e5a73806f5c
maxicuyo94 <maxibc94@gmail.com>
2021-10-31 23:48:06 -0300

    ready to  production

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const productsRouter = require('./products.routes');
+const categoriesRouter = require('./categories.routes');
+const usersRouter = require('./users.routes');
+const orderRouter = require('./orders.routes');
+const customersRouter = require('./customers.routes');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
+  router.use('/customers', customersRouter);
+}
+
+module.exports = routerApi;
